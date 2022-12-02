26a2c691291fb23fd2d0a6a21fb3f3ced6d84ce4
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2022-12-01 18:09:31 -0800

    Remove extra create-react-app files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import { useState } from "react";
-import TodoList from "./TodoList";
-import TodoEntryForm from "./TodoEntryForm";
+import { useState } from 'react';
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import TodoList from './TodoList'
+import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { id: 1, content: "Meet Masao for Udon" },
-    { id: 2, content: "Fix up these horrible React notes" }
+    {id: 1, content: 'Meet Masao for Udon'},
+    {id: 2, content: 'Fix up these horrible React notes'},
   ]);
   function deleteTodo(id) {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    setTodos(todos => todos.filter(todo => todo.id !== id));
   }
 
   function addTodo(todo) {
-    setTodos((todos) => [...todos, todo]);
+    setTodos(todos => [...todos, todo]);
   }
-
-  function submit(e) {
-    e.preventDefault();
-    if (content.trim()) {
-      adder({ id: Math.random(), content });
-    }
-    setContent("");
-  }
-
+  
   return (
-    <div className="App">
-      <h1>Things To Do</h1>
-      <TodoList todos={todos} deleter={deleteTodo} />
-      <TodoEntryForm adder={addTodo} />
-    </div>
+    <Container maxWidth="sm" style={{marginTop: 20}}>
+      <Paper style={{padding: 20, backgroundColor: '#fffff3'}}>
+        <Typography variant="h3" align="center">Things To Do</Typography>
+        <TodoList todos={todos} deleter={deleteTodo}/>
+        <TodoEntryForm adder={addTodo} />
+      </Paper>
+    </Container>
   );
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,9 +1,22 @@
-export default function TodoList({ todos }) {
-    const todoList = todos.length ? (
-      todos.map((todo) => <div key={todo.id}>{todo.content}</div>)
-    ) : (
-      <p>No todos left</p>
-    );
-    return <div>{todoList}</div>;
-  }
-  
+import { Fragment } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+
+export default function TodoList({ todos, deleter }) {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <Fragment key={todo.id}>
+          <ListItem>
+            <span onClick={() => deleter(todo.id)}>{todo.content}</span>
+          </ListItem>
+          <Divider />
+        </Fragment>
+      );
+    })
+  ) : (
+    <p>No todos left</p>
+  );
+  return <List style={{marginTop: 20}}>{todoList}</List>;
+}
